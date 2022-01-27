df8480f812f1073f351975be000c707263ebd53c
donlouie <don_gallegos@outlook.com>
2022-01-27 22:55:47 +0800

    finish p1

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom';
+
+const Footer = () => {
+	return (
+		<footer>
+			<p>Copyrigt &copy; 2022</p>
+			<Link href="/about"></Link>
+		</footer>
+	);
+};
+
+export default Footer;
